1c11dded5d476697690597351b9bcccfeb696980
Maneesh <56780894+maneesh06@users.noreply.github.com>
2020-09-24 12:13:17 +0530

    Add files via upload

---
+++ SIGNS_Recognition_Using_Tensorflow/TensorFlow_Model.py
@@ -0,0 +1,418 @@
+
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+##############################################################################
+
+get_ipython().magic('matplotlib inline')
+np.random.seed(1)
+
+
+
+
+# cost
+
+def cost(logits, labels):
+    """
+    Computes the cost using the sigmoid cross entropy
+    
+    Arguments:
+    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
+    labels -- vector of labels y (1 or 0) 
+    
+    
+    Returns:
+    cost -- runs the session of the cost 
+    """
+    
+    # Create the placeholders for "logits" (z) and "labels" (y)
+    z = tf.placeholder(tf.float32, name = "z")
+    y = tf.placeholder(tf.float32, name = "y")
+    
+    # Use the loss function 
+    cost = tf.nn.sigmoid_cross_entropy_with_logits(logits = z,  labels = y)
+    
+    # Create a session
+    sess = tf.Session()
+    
+    # Run the session 
+    cost = sess.run(cost, feed_dict = {z:logits, y:labels})
+    
+    # Close the session
+    sess.close()
+    
+    return cost
+
+
+#################################################################################################
+
+
+
+# one_hot_matrix
+
+def one_hot_matrix(labels, C):
+    """
+    Creates a matrix where the i-th row corresponds to the ith class number and the jth column
+                     corresponds to the jth training example. So if example j had a label i. Then entry (i,j) 
+                     will be 1. 
+                     
+    Arguments:
+    labels -- vector containing the labels 
+    C -- number of classes, the depth of the one hot dimension
+    
+    Returns: 
+    one_hot -- one hot matrix
+    """
+    
+    # Create a tf.constant equal to C (depth), name it 'C'.
+    C = tf.constant(C, name = 'C')
+    
+    # Use tf.one_hot, be careful with the axis 
+    one_hot_matrix = tf.one_hot(labels, C, axis = 0)
+    
+    # Create the session 
+    sess = tf.Session()
+    
+    # Run the session 
+    one_hot = sess.run(one_hot_matrix)
+    
+    # Close the session 
+    sess.close()
+    
+    return one_hot
+
+#############################################################################################
+
+
+
+# ones
+
+def ones(shape):
+    """
+    Creates an array of ones of dimension shape
+    
+    Arguments:
+    shape -- shape of the array you want to create
+        
+    Returns: 
+    ones -- array containing only ones
+    """
+
+    # Create "ones" tensor using tf.ones(...).
+    ones = tf.ones(shape)
+    
+    # Create the session 
+    sess = tf.Session()
+    
+    # Run the session to compute 'ones'
+    ones = sess.run(ones)
+    
+    # Close the session 
+    sess.close()
+
+    return ones
+
+
+######################################################################################
+
+
+
+
+
+##################### - Problem statement: SIGNS Dataset #############################
+
+# One afternoon, with some friends we decided to teach our computers to decipher sign language. We spent a few hours taking pictures in front of a white wall and came up with the following dataset.
+
+# - **Training set**: 1080 pictures (64 by 64 pixels) of signs representing numbers from 0 to 5 (180 pictures per number).
+# - **Test set**: 120 pictures (64 by 64 pixels) of signs representing numbers from 0 to 5 (20 pictures per number).
+# 
+# Note that this is a subset of the SIGNS dataset. The complete dataset contains many more signs.
+
+
+
+
+
+# Loading the dataset
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+
+# Change the index below and run the cell to visualize some examples in the dataset.
+# Example of a picture
+index = 0
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+
+# As usual you flatten the image dataset, then normalize it by dividing by 255. On top of that, you will convert each label to a one-hot vector. Run the cell below to do so.
+
+# Flatten the training and test images
+X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+# Normalize image vectors
+X_train = X_train_flatten/255.
+X_test = X_test_flatten/255.
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6)
+Y_test = convert_to_one_hot(Y_test_orig, 6)
+
+print ("number of training examples = " + str(X_train.shape[1]))
+print ("number of test examples = " + str(X_test.shape[1]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+# **Note** : that 12288 comes from 64x64x3. Each image is square, 64 by 64 pixels, and 3 is for the RGB colors. Please make sure all these shapes make sense to you before continuing.
+
+# **The model** : is *LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX*. The SIGMOID output layer has been converted to a SOFTMAX. A SOFTMAX layer generalizes SIGMOID to when there are more than two classes. 
+
+
+
+# Create placeholders
+def create_placeholders(n_x, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    
+    Arguments:
+    n_x -- scalar, size of an image vector (num_px * num_px = 64 * 64 * 3 = 12288)
+    n_y -- scalar, number of classes (from 0 to 5, so -> 6)
+    
+    Returns:
+    X -- placeholder for the data input, of shape [n_x, None] and dtype "tf.float32"
+    Y -- placeholder for the input labels, of shape [n_y, None] and dtype "tf.float32"
+    
+    """
+
+
+    X = tf.placeholder(tf.float32, shape=(n_x, None))
+    Y = tf.placeholder(tf.float32, shape=(n_y, None))    
+    return X, Y
+
+
+
+#################################################################################################
+
+# Initializing the parameters
+
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [25, 12288]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [6, 12]
+                        b3 : [6, 1]
+    
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+    
+    tf.set_random_seed(1)                   # so that your "random" numbers match ours
+        
+  
+    W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12,25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b2 = tf.get_variable("b2", [12,1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6,12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b3 = tf.get_variable("b3", [6,1], initializer = tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+
+##################################################################################################
+
+
+#  - Forward propagation in tensorflow 
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    # Numpy Equivalents:
+    Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
+    A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, A1) + b2
+    A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
+    Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3, A2) + b3
+    
+    return Z3
+
+
+####################################################################################################
+
+
+# compute_cost 
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    # to fit the tensorflow requirement for tf.nn.softmax_cross_entropy_with_logits(...,...)
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    
+
+    # computing cost and also suming all training example
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+    
+    return cost
+
+
+#################################################################################################
+#################################################################################################
+#################################################################################################
+#################################################################################################
+
+
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
+          num_epochs = 1500, minibatch_size = 32, print_cost = True):
+    """
+    Implements a three-layer tensorflow neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SOFTMAX.
+    
+    Arguments:
+    X_train -- training set, of shape (input size = 12288, number of training examples = 1080)
+    Y_train -- test set, of shape (output size = 6, number of training examples = 1080)
+    X_test -- training set, of shape (input size = 12288, number of training examples = 120)
+    Y_test -- test set, of shape (output size = 6, number of test examples = 120)
+    learning_rate -- learning rate of the optimization
+    num_epochs -- number of epochs of the optimization loop
+    minibatch_size -- size of a minibatch
+    print_cost -- True to print the cost every 100 epochs
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    
+    ops.reset_default_graph()                         # to be able to rerun the model without overwriting tf variables
+    tf.set_random_seed(1)                             # to keep consistent results
+    seed = 3                                          # to keep consistent results
+    (n_x, m) = X_train.shape                          # (n_x: input size, m : number of examples in the train set)
+    n_y = Y_train.shape[0]                            # n_y : output size
+    costs = []                                        # To keep track of the cost
+    
+    # Create Placeholders of shape (n_x, n_y)
+    X, Y = create_placeholders(n_x, n_y)
+    
+
+    # Initialize parameters
+    parameters = initialize_parameters()
+    
+    
+    # Forward propagation: Build the forward propagation in the tensorflow graph
+    Z3 = forward_propagation(X, parameters)
+
+    
+    # Cost function: Add cost function to tensorflow graph
+    cost = compute_cost(Z3, Y)
+    
+    
+    # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer.
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
+ 
+    
+    # Initialize all the variables
+    init = tf.global_variables_initializer()
+
+    # Start the session to compute the tensorflow graph
+    with tf.Session() as sess:
+        
+        # Run the initialization
+        sess.run(init)
+        
+        # Do the training loop
+        for epoch in range(num_epochs):
+
+            epoch_cost = 0.                       # Defines a cost related to an epoch
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            seed = seed + 1
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+
+                # Select a minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+                
+                # IMPORTANT: The line that runs the graph on a minibatch.
+                # Run the session to execute the "optimizer" and the "cost", the feedict should contain a minibatch for (X,Y).
+                _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+                
+                
+                epoch_cost += minibatch_cost / minibatch_size
+
+            # Print the cost every epoch
+            if print_cost == True and epoch % 100 == 0:
+                print ("Cost after epoch %i: %f" % (epoch, epoch_cost))
+            if print_cost == True and epoch % 5 == 0:
+                costs.append(epoch_cost)
+                
+        # plot the cost
+        plt.plot(np.squeeze(costs))
+        plt.ylabel('cost')
+        plt.xlabel('iterations (per fives)')
+        plt.title("Learning rate =" + str(learning_rate))
+        plt.show()
+
+        # lets save the parameters in a variable
+        parameters = sess.run(parameters)
+        print ("Parameters have been trained!")
+
+        # Calculate the correct predictions
+        correct_prediction = tf.equal(tf.argmax(Z3), tf.argmax(Y))
+
+        # Calculate accuracy on the test set
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print ("Train Accuracy:", accuracy.eval({X: X_train, Y: Y_train}))
+        print ("Test Accuracy:", accuracy.eval({X: X_test, Y: Y_test}))
+        
+        return parameters
+
+
+# Run the following cell to train your model! On our machine it takes about 5 minutes. Your "Cost after epoch 100" should be 1.048222.
+
+parameters = model(X_train, Y_train, X_test, Y_test)
+
+
+
+# Amazing, your algorithm can recognize a sign representing a figure between 0 and 5 with 71.7% accuracy.
+
+
